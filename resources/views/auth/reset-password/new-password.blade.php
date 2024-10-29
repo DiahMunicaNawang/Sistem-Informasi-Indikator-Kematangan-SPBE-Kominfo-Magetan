cf9e219ae4b8a9852ec84a9f11c1b7dbdf73cc22
ibrakusuma <ibrahimrama3123@gmail.com>
2024-10-29 23:08:08 +0700

    style(layout): restyling reset password page

---
+++ resources/views/auth/reset-password/new-password.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.auth.index')
+
+@section('content')
+
+@if (session('status'))
+    <div class="alert alert-success">{{ session('status') }}</div>
+@endif
+
+<form class="form w-100" action="{{ route('password.email') }}" method="POST">
+    @csrf
+    <!--begin::Heading-->
+    <div class="mb-10 text-center">
+        <!--begin::Title-->
+        <h1 class="mb-3 text-dark fw-bolder">Lupa Password ?</h1>
+        <!--end::Title-->
+        <!--begin::Link-->
+        <div class="text-gray-500 fw-semibold fs-6">Masukkan username Anda untuk mereset password.</div>
+        <!--end::Link-->
+    </div>
+    <!--begin::Heading-->
+    <!--begin::Input group=-->
+    <div class="mb-8 fv-row">
+        <!--begin::Username-->
+        <label for="username">Username</label>
+        <input type="text" name="username" autocomplete="off" class="bg-transparent form-control" id="username" required/>
+        <!--end::Username-->
+    </div>
+    <!--begin::Actions-->
+    <div class="flex-wrap d-flex justify-content-center pb-lg-0">
+        <button type="submit" class="btn btn-primary me-4">
+            Reset
+        </button>
+        <a href="{{ route("login") }}" class="btn btn-light">Cancel</a>
+    </div>
+    <!--end::Actions-->
+</form>
+
+@endsection
